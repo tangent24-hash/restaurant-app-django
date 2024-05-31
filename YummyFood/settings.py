76e4d6448d16ae1490b077c593856dd785418640
Sohan <sohanur471@gmail.com>
2024-05-31 23:06:08 +0600

    env

--- YummyFood/settings.py
+++ YummyFood/settings.py
@@ -11,7 +11,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
